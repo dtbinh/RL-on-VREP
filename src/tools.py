f415754f1adf881872a2ec099f19049554af85e5
machaneus <machaneus@gmail.com>
2015-10-02 14:34:58 +0300

    Implement LinearApproximator class and relevant methods

--- src/tools.py
+++ src/tools.py
@@ -1,4 +1,46 @@
+import math
+import numpy as np
+
 class ZeroDict(dict):
     def __getitem__(self, key):
         self.setdefault(key, 0)
         return dict.__getitem__(self, key)
+
+class LinearApproximator:
+    def __init__(self, actionSet, targetPos):
+        
+        self.actionSet = actionSet        
+        self.weights = dict()
+        
+        for action in actionSet:
+            self.weights[action] = np.zeros(3)
+        
+        self.targetPos = targetPos
+    
+    def f(self, state):
+        
+        f1 = abs(self.targetPos[0]-state[0])+abs(self.targetPos[1]-state[1])
+        f2 = abs(math.atan2(self.targetPos[1]-state[1],self.targetPos[0]-state[0]) - state[2])
+        f3 = state[3]
+        
+        return np.array([f1,f2,f3])
+        
+    def calculateQValue(self, state, action):
+        return np.dot(self.weights[action],self.f(state))
+        
+    def getBestActionMaxQValue(self, state):
+        bestAction = None
+        maxQValue=-10000000
+        for action in self.actionSet:
+            qValue=self.calculateQValue(state, action)
+            if maxQValue<qValue:
+                maxQValue=qValue
+                bestAction = action
+        
+        return bestAction, maxQValue
+    
+    def updateWeights(self, state, action, newState, reward, discount, alpha):
+        _,maxNextQValue = self.getBestActionMaxQValue(newState)
+        difference = (reward+discount*maxNextQValue)-self.calculateQValue(state,action)
+        self.weights[action] = self.weights[action] - alpha*difference*self.f(state)
+
