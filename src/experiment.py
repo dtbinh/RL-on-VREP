89debe2bb8f210ccfb2dd09a37876a53392040cc
machaneus <machaneus@gmail.com>
2015-10-04 11:55:07 +0300

    Change experiment to apply Approximate Q-learning with debugging prints

--- src/experiment.py
+++ src/experiment.py
@@ -5,23 +5,45 @@ Created on Mon Sep 21 14:58:48 2015
 @author: Netbook
 """
 
-from qLearningAgent import QLearningAgent
-from Environment import SimpleEnvironment
+#from qLearningAgent import QLearningAgent
+#from Environment import SimpleEnvironment
+#
+#env = SimpleEnvironment()
+#agent = QLearningAgent(0.2,0.4)
+#
+#for k in range(1000):
+#    #print 20*'='
+#    state = env.getState()
+#    #print "Current state: ", state
+#    action=agent.selectAction(state)
+#    #print "Selected action: ", action
+#    newState,reward=env.applyAction(action)
+#    #print "New state: ",newState, "\tReward: ",reward
+#    agent.update(state,action,newState,reward)
+#    #print 'Updated Q[',state,',',action,']: ', agent.QValues[(state,action)]
+#    
+#print 'Q[(2,4),(-1,-1)]: ', agent.QValues[(2,4),(-1,-1)]
+#print 'Q[(1,1),(-1,-1)]: ', agent.QValues[(1,1),(-1,-1)]
 
-env = SimpleEnvironment()
-agent = QLearningAgent(0.2,0.4)
+from qLearningAgent import ApproximateQLearningAgent
+from environment import MapStraight
 
-for k in range(1000):
-    #print 20*'='
+env = MapStraight()
+agent = ApproximateQLearningAgent(0.5,0.01,1,env.target)
+env.start()
+
+for k in range(50):
+    print 10*'='," iteration: ",k,10*'='
     state = env.getState()
-    #print "Current state: ", state
+    print "Current state: ", state
     action=agent.selectAction(state)
-    #print "Selected action: ", action
+    print "Selected action: ", action
     newState,reward=env.applyAction(action)
-    #print "New state: ",newState, "\tReward: ",reward
+    print "New state: ",newState, "\tReward: ",reward
     agent.update(state,action,newState,reward)
-    #print 'Updated Q[',state,',',action,']: ', agent.QValues[(state,action)]
-    
-print 'Q[(2,4),(-1,-1)]: ', agent.QValues[(2,4),(-1,-1)]
-print 'Q[(1,1),(-1,-1)]: ', agent.QValues[(1,1),(-1,-1)]
+    print 'Updated weights[',action,']: ', agent.QValues.weights[action]
 
+env.stop()
+    
+#print 'Q[(2,4),(-1,-1)]: ', agent.QValues[(2,4),(-1,-1)]
+#print 'Q[(1,1),(-1,-1)]: ', agent.QValues[(1,1),(-1,-1)]
